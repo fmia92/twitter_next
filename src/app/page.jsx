bf96dcc30ba7050ba94dddebfba441debda0ea20
fran sola <fmia1992@gmail.com>
2023-08-16 10:47:10 +0200

    .

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export default async function Home () {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: posts } = await supabase.from('posts').select('*')
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      hola twitter
+      {
+        JSON.stringify(posts)
+      }
+    </main>
+  )
+}
